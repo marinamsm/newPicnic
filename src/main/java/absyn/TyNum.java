c796076ff3f37e9974255f45727258347b4146cd
Marina <marinamsm@live.com>
2018-11-20 23:39:15 -0200

    TyNum for num/double

--- src/main/java/absyn/TyNum.java
+++ src/main/java/absyn/TyNum.java
@@ -1,4 +1,29 @@
 package absyn;
 
-public class TyNum {
+import env.Env;
+import io.vavr.collection.Tree;
+import types.INT;
+import types.Type;
+
+import env.Env;
+import io.vavr.collection.Tree;
+import types.NUM;
+import types.Type;
+
+public class TyNum extends Ty {
+
+    public TyNum(Loc loc) {
+        super(loc);
+    }
+
+    @java.lang.Override
+    public Tree.Node<String> toTree() {
+        return Tree.of("num");
+    }
+
+    @Override
+    public Type semantic(Env env) {
+        return NUM.T;
+    }
 }
+
